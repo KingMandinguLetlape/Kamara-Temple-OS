db52555df2a0294beb08beceb982fb704461347b
KingMandinguLetlape <oteebusy112@gmail.com>
2025-08-06 19:35:15 +0200

    Update container.js

--- components/ui/container.js
+++ components/ui/container.js
@@ -1,6 +1,6 @@
-export function Container({ children }) {
+export default function Container({ children }) {
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="max-w-2xl mx-auto p-4">
       {children}
     </div>
   );
