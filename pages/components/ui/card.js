0ff21eac2537169f7188c74c3bef5e11745857a6
KingMandinguLetlape <oteebusy112@gmail.com>
2025-08-06 11:00:20 +0200

    Update card.js

--- pages/components/ui/card.js
+++ pages/components/ui/card.js
@@ -1,8 +1,6 @@
 export function Card({ children, className = "" }) {
   return (
-    <div
-      className={`bg-white dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 rounded-2xl p-4 shadow-lg ${className}`}
-    >
+    <div className={`bg-zinc-900/50 border border-zinc-800 rounded-2xl shadow-md p-4 ${className}`}>
       {children}
     </div>
   );
