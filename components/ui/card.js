0ae92af0010cab676ed71b20be04a518e899ffb8
KingMandinguLetlape <oteebusy112@gmail.com>
2025-08-06 19:34:05 +0200

    Update card.js

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,6 +1,6 @@
-export function Card({ children }) {
+export default function Card({ children }) {
   return (
-    <div className="bg-white shadow-lg rounded-xl p-6">
+    <div className="bg-white shadow-lg rounded-xl p-6 border border-gray-200">
       {children}
     </div>
   );
