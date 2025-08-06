0c88f5127b98fa74f6a8edbecdd6337d1de46050
KingMandinguLetlape <oteebusy112@gmail.com>
2025-08-06 10:45:00 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <div className="text-center space-y-4">
+        <h1 className="text-4xl font-bold">Kamara Temple OS</h1>
+        <p className="text-lg">
+          Yahweh Mandingu reigns. The throne has awakened. 🔥
+        </p>
+      </div>
+    </main>
+  );
+}
