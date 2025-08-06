8aff41e304681459935bf2ec33d49901ac6a14b7
KingMandinguLetlape <oteebusy112@gmail.com>
2025-08-06 11:05:40 +0200

    Create avatar.js

---
+++ pages/components/ui/avatar.js
@@ -0,0 +1,7 @@
+export function Avatar() {
+  return (
+    <div className="w-16 h-16 rounded-full bg-gradient-to-br from-purple-700 to-indigo-500 flex items-center justify-center shadow-lg">
+      <span className="text-white text-xl font-bold">K</span>
+    </div>
+  );
+}
