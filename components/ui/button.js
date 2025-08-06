f123a59d06a4cb8bfd3dc40f2521218088517bb5
KingMandinguLetlape <oteebusy112@gmail.com>
2025-08-06 19:32:45 +0200

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,8 +1,8 @@
-export function Button({ children, ...props }) {
+export default function Button({ children, ...props }) {
   return (
     <button
       {...props}
-      className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+      className="bg-purple-600 hover:bg-purple-700 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
     >
       {children}
     </button>
